6a5676eb62bcde72d223bcf3f93e62f8bc45b18a
PravinDevMaster <pravin2001thanasingh@gmail.com>
2025-11-03 13:56:00 +0530

    complete the art animation

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from "./components/Hero";
 import { CSSPlugin, ScrollTrigger, SplitText } from "gsap/all";
 import Cocktails from "./components/Cocktails";
 import About from "./components/About";
+import Art from "./components/Art";
 
 gsap.registerPlugin(ScrollTrigger, SplitText, CSSPlugin);
 
@@ -14,6 +15,7 @@ const App = () => {
       <Hero />
       <Cocktails />
       <About />
+      <Art />
       {/* <div className="h-dvh bg-black"></div> */}
     </main>
   );

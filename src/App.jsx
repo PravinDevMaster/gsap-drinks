ec95c67acee7102ac7edae04461df2e2616d1fba
PravinDevMaster <pravin2001thanasingh@gmail.com>
2025-11-03 13:29:48 +0530

    completed the about UI

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import { CSSPlugin, ScrollTrigger, SplitText } from "gsap/all";
 import Cocktails from "./components/Cocktails";
+import About from "./components/About";
 
 gsap.registerPlugin(ScrollTrigger, SplitText, CSSPlugin);
 
@@ -12,7 +13,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <Cocktails />
-
+      <About />
       {/* <div className="h-dvh bg-black"></div> */}
     </main>
   );
